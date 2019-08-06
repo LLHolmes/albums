2e4fbbb3fbc7089fb30e3434f9ff2e1b3820685f
LLHolmes <lana.lee.holmes@gmail.com>
2019-08-06 14:53:14 -0500

    Create Header component and nested inside App

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: "rallycoding"
+  extends: "rallycoding",
+  "rules": {
+    "react/require-extension": "off"
+  }
 };
--- index.ios.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class test1 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-    )
-  }
-}
